553f7b99a6c0a9aac2e70b5276f32b4eb2c8b81b
Mark <mark.cherrie.mc@gmail.com>
2022-11-18 11:58:09 +0000

    time use by occ graphs by time period (covid/no-cov)

--- timebyocc_20162021.R
+++ timebyocc_20162021.R
@@ -46,8 +46,44 @@ d<-cbind(as.data.frame(checklabels),as.data.frame(Palll))
 #write.csv(d, "data/colourcodebook.csv", row.names=F)
 
 
+df$value[df$value==201]<-"Home"
+df$value[df$value==202]<-"Work"
+df$value[df$value==203]<-"Other"
 
-checkgroups<-tidyr::pivot_wider(df, id_cols=c(survey, mainid, dday), names_from=name,
+df$occgroup[df$occgroup==1]<-"1-Semi or unskilled manual work"
+df$occgroup[df$occgroup==2]<-"2-Skilled manual worker"
+df$occgroup[df$occgroup==3]<-"3-Administrative (not managerial or supervisory responsibility)"
+df$occgroup[df$occgroup==4]<-"4-Supervisory/ junior managerial"
+df$occgroup[df$occgroup==5]<-"5-Intermediate managerial/ professional/ administrative"
+df$occgroup[df$occgroup==6]<-"6-Higher managerial/ professional/ administrative"
+df$occgroup[df$occgroup==-7]<-"Not applicable"
+
+
+df$survey[df$survey!=1]<-"Covid"
+df$survey[df$survey==1]<-"Pre-Covid"
+
+df$survey<-factor(df$survey, levels=c("Pre-Covid", "Covid"))
+
+########################################################
+
+
+df_subset <- df %>% filter(dday > 1 & dday <=7) %>%
+  filter(occgroup=="1-Semi or unskilled manual work" |
+           occgroup=="2-Skilled manual worker") 
+
+df_subset <- df %>% filter(dday > 1 & dday <=7) %>%
+  filter(occgroup=="3-Administrative (not managerial or supervisory responsibility)" |
+           occgroup=="4-Supervisory/ junior managerial") 
+
+df_subset <- df %>% filter(dday > 1 & dday <=7) %>%
+  filter(occgroup=="5-Intermediate managerial/ professional/ administrative" |
+           occgroup=="6-Higher managerial/ professional/ administrative") 
+
+
+
+#df_subset <- df
+
+checkgroups<-tidyr::pivot_wider(df_subset, id_cols=c(survey, mainid, dday, occgroup), names_from=name,
                                 values_from = value)
 
 timelist<-format( seq.POSIXt(as.POSIXct(Sys.Date()), as.POSIXct(Sys.Date()+1), by = "10 min"),
@@ -55,10 +91,41 @@ timelist<-format( seq.POSIXt(as.POSIXct(Sys.Date()), as.POSIXct(Sys.Date()+1), b
 timelist<-c(timelist[-1:-24][1:120],timelist[1:24])
 
 
-library("TraMineR")
-mvad.alphab <- checkgroups_labels
-mvad.seq <- seqdef(checkgroups, 4:147, alphabet = c(201, 202,203))
 
-seqdplot(mvad.seq, border = NA, with.legend=FALSE, xtlab=timelist)
+library("TraMineR")
+
+mvad.seq <- seqdef(checkgroups, 5:148, alphabet = c("Home", "Other","Work"))
+
+seqdplot(mvad.seq, border = NA, with.legend=T, xtlab=timelist, group=checkgroups$survey)
+
+
+seqHtplot(mvad.seq, group=checkgroups$survey, xtlab=timelist, xlab="Time (24hr)")
+
+
+
+#seqmtplot(mvad.seq, with.legend = T, group=checkgroups$survey)
+
+#mvad.om <- seqdist(mvad.seq, method = "OM", indel = 1, sm = "TRATE")
+
+# 201 is home
+# 202 is work
+# 203 is other
+
+# build an entropy model
+
+df_subset <- df %>% filter(dday > 1 & dday <=7)
+checkgroups<-tidyr::pivot_wider(df_subset, id_cols=c(survey, mainid, dday, occgroup), names_from=name,
+                                values_from = value)
+
+timelist<-format( seq.POSIXt(as.POSIXct(Sys.Date()), as.POSIXct(Sys.Date()+1), by = "10 min"),
+                  "%H%M", tz="GMT")
+timelist<-c(timelist[-1:-24][1:120],timelist[1:24])
+mvad.seq <- seqdef(checkgroups, 5:148, alphabet = c("Home", "Other","Work"))
+
+### model
+entropies <- seqient(mvad.seq)
+lm.ent <- lm(entropies ~ occgroup, checkgroups)
+summary(lm.ent)
+
 
 
